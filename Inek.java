207527f26c9401694175e953dc69d3814d9b9e4f
samethaz <74431850+samethaz@users.noreply.github.com>
2020-12-04 00:49:41 +0100

    Update Inek.java

--- Inek.java
+++ Inek.java
@@ -1,26 +1,26 @@
-package ciftlik;
+package ciftci;
+import ciftlik.*;
 
+public class ciftci
+{
 
-public class Inek
-{   static int  Sinek=0;
-    public String isim;
-    public Inek()
+    public static void main(String[] args)
     {
-        Sinek++;
-        isim="inek";
-        topla();
-        isim=isim+ Sinek;
-    }
-    public Inek(String isim)
-    {
-        Sinek++;
-        topla();
-        this.isim=isim+ Sinek;
-    }
-    public static int topla(){
+    Tavuk t1=new Tavuk();
+    System.out.println(t1.isim);
+    Inek i1=new Inek();
+    System.out.println(i1.isim);
+    Tavuk t2=new Tavuk("ali");
+    System.out.println(t2.isim);
+    Inek i2=new Inek("veli");
+    System.out.println(i2.isim);
+    Inek i3=new Inek("ahmet");
+    System.out.println(i2.isim);
+    Inek i4=new Inek("ahmet");
+    System.out.println(i4.isim);
+        System.out.println("Tavuk sayısı.......:"+Tavuk.Stavuk);
+        System.out.println("İnek sayısı.......:"+Inek.Sinek);
+    System.out.println("Hayvan sayısı.......:"+Tavuk.topla());
 
-        return Tavuk.Stavuk+Inek.Sinek;
-
-    }
 }
-
+}
